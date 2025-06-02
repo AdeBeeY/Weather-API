d10e19d00848891de7df433f5b89ca5e17c476bd
Adebayo Adewusi <adewusiadebayo@gmail.com>
2025-06-02 16:01:33 +0100

    validate the form with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+import { getLocation } from "./api.js";
+
+
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+let userLocation = document.getElementById('location');
+const error = document.querySelector('.error');
+
+
+
+button.addEventListener('click', () => {
+  let userLocationTrimed = userLocation.value.trim();
+  if(userLocationTrimed === '') {
+      // Toggles the active and prevent the user from submitting an empty string
+    error.classList.toggle('active');
+    error.textContent = "The field above can't be left empty!";
+    return
+  } else {
+    console.log(userLocationTrimed)
+    // return userLocationTrimed;
+    getLocation(userLocationTrimed);
+  }
+})
+
+form.addEventListener('click', (e) => {
+  e.preventDefault();
+} )
